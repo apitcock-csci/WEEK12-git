18df0179519f019389db1f83fd2c6881b1147491
Anna Pitcock <annapitcock@csm-wl-dhcp-225-57.mines.edu>
2019-11-20 14:06:24 -0700

    Created third function

--- Week12-utility.py
+++ Week12-utility.py
@@ -5,9 +5,17 @@
 
 #Function 1
 def PrintOutput(string_to_output):
-    print(string_to_output)
+    print("OUTPUT", string_to_output)
 
+#Function 2
 def LoadFile(input_string):
     with open(input_string, 'r') as file:
         read_file = file.readlines()
-        print(read_file)
+        print("OUTPUT", read_file)
+        
+#Function 3
+#FIX ME
+def UpdateString(string_1, new_letter, index_num):
+    old_letter = string_1[index_num]
+    string_1.replace(old_letter, new_letter)
+    print(string_1)
