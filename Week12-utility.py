c7450848db7c8e065c1d7e1e90a9fe377f7e691e
Anna Pitcock <annapitcock@csm-wl-dhcp-225-57.mines.edu>
2019-11-20 21:42:39 -0700

    Created 4th function

--- Week12-utility.py
+++ Week12-utility.py
@@ -10,7 +10,7 @@ def PrintOutput(string_to_output):
 #Function 2
 def LoadFile(input_string):
     with open(input_string, 'r') as file:
-        read_file = file.readlines()
+        read_file = file.read().splitlines()
         print("OUTPUT", read_file)
         
 #Function 3
@@ -19,3 +19,11 @@ def UpdateString(string_1, new_letter, index_num):
     old_letter = string_1[index_num]
     string_1.replace(old_letter, new_letter)
     print(string_1)
+
+#Funtion 4
+def FindWordCount(input_list, input_string):
+    occurances = 0
+    for i in list:
+        if i == input_string:
+            occurances += 1
+    print("OUTPUT", occurances)
